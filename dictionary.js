30c73fa79c53e464c18c5b93e769d74fb80da4b5
CJ Cameron <96845401+CJCameron13@users.noreply.github.com>
2023-12-21 17:26:26 -0500

    Update dictionary.js

--- dictionary.js
+++ dictionary.js
@@ -1,6 +1,9 @@
 const form = document.querySelector('#input-form')
 const word = document.querySelector('#word')
 const pronunciation = document.querySelector('#pronunciation')
+const partOfSpeechOne = document.querySelector("#pos-one")
+const definitionList = document.querySelector('#definition-list')
+const synonyms = document.querySelector('.synonyms')
 
 function submitForm(event){
 
@@ -10,6 +13,19 @@ function submitForm(event){
 
 form.addEventListener('submit', submitForm)
 
+function addItem(def) {
+    let newItem = document.createElement('li')
+    newItem.className = 'definition'
+    definitionList.appendChild(newItem)
+    newItem.textContent = def
+}
+
+const callFunction = (func, n) => {
+    for (let i = 1; i <= n; i++) {
+        func()
+    }
+}
+
 form.addEventListener('submit', () => {
     console.log('submitted')
     const userInput = document.querySelector('#user-input').value
@@ -18,9 +34,22 @@ form.addEventListener('submit', () => {
     fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${userInput}`)
 .then(response => response.json())
 .then(data => {
-    console.log(data);
+    console.log(data[0].meanings[0].synonyms);
     word.textContent = data[0].word
     pronunciation.textContent = data[0].phonetic
+    partOfSpeechOne.textContent = data[0].meanings[0].partOfSpeech
+    
+        let i = 0
+        while (i < data[0].meanings[0].definitions.length) {
+            console.log(data[0].meanings[0].definitions[i].definition)
+            addItem(data[0].meanings[0].definitions[i].definition)
+            i++
+        }
+    
+    synonyms.textContent = data[0].meanings[0].synonyms.join(', ')
+    
 });
 
 })
+
+// `${data[0].meanings[0].definitions[def].definition}`
